13479b0d6d080c3c205dc4abdbe3f409f9faa643
IsaacFigueiredo-code <izaque935@gmail.com>
2025-02-08 19:11:31 +0000

    supertrunfo_

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -8,7 +8,6 @@
 
 int main() {
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    char codigo_estado;
     char codigo_cidade;
     int populacao;
     float area;
@@ -19,18 +18,22 @@ int main() {
     // Cadastro das Cartas:
     // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
     // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    scanf("Insira o código do estado (letras de A a H): %c\n", codigo_estado);
-    scanf("Insira o código da cidade (numeradas de 1 a 4): %c\n", codigo_cidade);
-    scanf("Insira a população da cidade: %i\n", populacao);
-    scanf("Insira a área da cidade em metros quadrados: %f\n", area);
-    scanf("Insira o PIB da cidade em R$(reais): %f\n", PIB);
-    scanf("Insira o números de pontos turísticos da cidade: %i\n", pontos_turisticos);
+    printf("Insira o código do estado e código da cidade combinado (A1, como exemplo): \n");
+    scanf("%c", &codigo_cidade);
+    printf("\nInsira a população da cidade: \n");
+    scanf("%i", &populacao);
+    printf("\nInsira a área da cidade em metros quadrados: \n");
+    scanf("%f", &area);
+    printf("\nInsira o PIB da cidade em R$(reais): \n");
+    scanf("%f", &PIB);
+    printf("\nInsira o números de pontos turísticos da cidade: \n");
+    scanf("%i", &pontos_turisticos);
 
     
     // Exibição dos Dados das Cartas:
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
-    printf("O código da carta é %c+%c\n", codigo_estado, codigo_cidade);
+    printf("O código da carta é %c\n", codigo_cidade);
     printf("A população da cidade é de %i habitantes\n", populacao);
     printf("A área da cidade é de %f metros quadrados\n", area);
     printf("A cidade possui um PIB de %f reais\n", PIB);
